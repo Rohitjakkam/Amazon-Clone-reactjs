c862a9486cc7b0121435274cb4ac21db3a892585
Rohit Jakkam <86354086+Rohitjakkam@users.noreply.github.com>
2023-07-08 23:22:37 +0530

    Update Product.js
    add to basket

--- Product.js
+++ Product.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import "./Product.css";
+import "./Product.css"; 
+import { useStateValue } from './StateProvider';
 
 
 function Product({id,title,image,price,rating}) {
+    const [{},dispatch]=useStateValue();
+    const addToBasket = () =>{
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item:{
+                id:id,
+                title:title,
+                image:image,
+                price:price,
+                rating:rating,
+
+
+            },
+
+        });
+    };
+
   return (
     <div className="product">
         <div className="product__info">
@@ -22,7 +40,7 @@ function Product({id,title,image,price,rating}) {
         
 
         <img src={image} alt="" />
-        <button>Add to basket</button>
+        <button onClick={addToBasket}>Add to basket</button>
 
     </div>
   )
