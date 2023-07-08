d7085599a5b0352c109de1dbf63b6b1fa156cf55
Rohit Jakkam <86354086+Rohitjakkam@users.noreply.github.com>
2023-07-08 23:21:35 +0530

    Update App.js
    react router all set

--- App.js
+++ App.js
@@ -1,29 +1,20 @@
 import React from "react";
-import {BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from "./Header.js";
 import Home from "./Home.js"
+import Checkout from "./Checkout";
 
 function App() {
   return (
     <Router>
-        <div className="app">
-          {/* <Routes>
-            <Route path="/checkout">
-              <h1>Checkout page</h1>
-            </Route>
-            <Route path="/login">
-              <h1>login page</h1>
-            </Route>
-            <Route path="/">
-              <Header />
-              <h1>Home page</h1>
-            </Route>
-          </Routes> */}
-           <Header />
-           <Home />
-        
-    </div>
+      <div className="app">
+        <Routes>
+        <Route path="/" element={<React.Fragment><Header /><Home /></React.Fragment>} />
+        <Route path="/checkout" element={<React.Fragment><Header /><Checkout /></React.Fragment>} />
+        {/* <Route path="/contact" element={} /> */}
+        </Routes>    
+      </div>
     </Router>
     
   );
