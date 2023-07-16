d1f239187bdc609775d06ce628fe229d8c156cc2
Rohit Jakkam <86354086+Rohitjakkam@users.noreply.github.com>
2023-07-16 23:16:24 +0530

    Update reducer.js
    remaining

--- reducer.js
+++ reducer.js
@@ -1,4 +1,5 @@
 
+
 export const initialState ={
     basket: [],
     user:null,
@@ -14,7 +15,19 @@ const reducer = (state ,action)=> {
             };
          
         case 'REMOVE_FROM_BASKET':
-            return {state};
+            let newBasket = [...state.basket];
+            const index = state.basket.findIndex((basketItem)=> basketItem.id ===action.id);
+
+            if (index >=0){
+                newBasket.splice(index,1);
+            }else {
+                console.warn(
+                    'Cant remove product (id:${action.id}) as its no'
+                );
+            }
+
+
+            return {...state};
    
         default:
             return state;
