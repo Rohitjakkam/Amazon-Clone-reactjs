361b89d6b34a0fa9c83c2e9ac70fb1bac6cca03d
Rohit Jakkam <86354086+Rohitjakkam@users.noreply.github.com>
2023-07-23 23:41:12 +0530

    Update Subtotal.js
    done

--- Subtotal.js
+++ Subtotal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
     const [{basket},dispatch]=useStateValue();
